6487cc22a138ba254f457a2498d64645e45e6db9
yudahan <103639913+danbi22@users.noreply.github.com>
2023-03-15 18:28:32 +0900

    java 문법 intro

---
+++ Java01_intro/src/edu/java/intro/IntroMain.java
@@ -0,0 +1,16 @@
+package edu.java.intro;
+
+public class IntroMain {
+	
+	/*
+	 * 블록 주석(block comment) 문서를 만들 때 사용
+	 */
+	
+	
+	// 한줄주석(in-line comment): 프로그램에 대한 설명. 컴파일할 때 무시됨.
+	// main method(메인 메서드): 프로그램의 시작.
+	public static void main(String[]args) {
+		System.out.println("안녕하세요, Java!");  // 콘솔창에 문장 출력.
+	}
+	
+}
