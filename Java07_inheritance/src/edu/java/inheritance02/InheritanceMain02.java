9bfcc371a2dd549a5a14ade36a8627de59062457
yudahan <103639913+danbi22@users.noreply.github.com>
2023-03-17 18:20:40 +0900

    hi

--- Java07_inheritance/src/edu/java/inheritance02/InheritanceMain02.java
+++ Java07_inheritance/src/edu/java/inheritance02/InheritanceMain02.java
@@ -9,6 +9,23 @@ public class InheritanceMain02 {
 		BusinessPerson p2 = new BusinessPerson("아이티윌");
 		System.out.println();
 		BusinessPerson p3 = new BusinessPerson("유다한", "아이티윌");
+		
+		// 하위 클래스의 객체를 생성하기 위해서 생성자를 호출하면
+		// 상위 클래스의 생성자가 먼저 실행되고, 그 후에 하위 클래스의 생성자가 실행됨
+		// 하위 클래스의 생성자에서 상위 클래스의 생성자를 호출하려면 super(...)를 사용하면 됨.
+		// 
+		// 하위 클래스의 생성자에서 상위 클래스의 생성자를 명시적으로 호출하지 않으면, 
+		// 상위 클래스의 "기본 생성자(default constructor)"가 암묵적으로 호출됨.
+		// 만약 상위 클래스에 기본 생정자가 없으면, 
+		// 하위 클래스에서는 상위 클래스의 다른 생성자를 반드시 명시적으로 호출해야함
+		
+		// this의 의미:
+		// 1. 생성된 (자기자신)객체의 주소: this.field, this.method()
+		// 2. 오버로딩된 다른 생성자: this(), this(...)
+		
+		// super의 의미:
+		// 1. 생성된 상위 클래스의 객체(의 주소): super.field, super.method()
+		// 2. 상위 클래스의 생성자: super(), super(...)
 	}
 
 }
