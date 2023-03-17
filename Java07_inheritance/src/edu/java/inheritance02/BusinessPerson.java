32ba0a9116ba5dcda588a77e437273f246d1b6a4
yudahan <103639913+danbi22@users.noreply.github.com>
2023-03-17 18:00:19 +0900

    Inheritance

--- Java07_inheritance/src/edu/java/inheritance02/BusinessPerson.java
+++ Java07_inheritance/src/edu/java/inheritance02/BusinessPerson.java
@@ -1,24 +1,29 @@
 package edu.java.inheritance02;
 
-import java.nio.file.spi.FileSystemProvider;
-
 // 회사원은 사람이다. 회사원은 사람을 확장한다.
 public class BusinessPerson extends Person {
 	// field
 	private String company;
 	
 	// constructor
+	// 상속된 하위 클래스에서 파라미터를 넣은 생성자를 생성하면 반드시 
 	public BusinessPerson() {
+//		super(); // 상위 클래스의 기본 생성자 호출은 생략 가능.
 		System.out.println("BusinessPerson() 생성자 호출.");
 	}
 	
 	public BusinessPerson(String company) {
+//		super();
 		this.company = company;
 		System.out.println("BusinessPerson(company) 생성자 호출.");
 	}
 	
+//	public BusinessPerson() {
+//		super();
+//	}
+
 	public BusinessPerson(String name, String company) {
-		super(name); // 부모의 생성자를 호출.
+		super(name); // 부모의 생성자를 호출. 명시적 호출
 		this.company = company;
 		System.out.println("BusinessPerson(name, company) 생성자 호출.");
 	}
--- Java07_inheritance/src/edu/java/inheritance02/InheritanceMain02.java
+++ Java07_inheritance/src/edu/java/inheritance02/InheritanceMain02.java
@@ -5,6 +5,10 @@ public class InheritanceMain02 {
 	public static void main(String[] args) {
 		// 상속 관계에서 생성자 호출 순서:
 		BusinessPerson p1 = new BusinessPerson();
+		System.out.println();
+		BusinessPerson p2 = new BusinessPerson("아이티윌");
+		System.out.println();
+		BusinessPerson p3 = new BusinessPerson("유다한", "아이티윌");
 	}
 
 }
