6ae94aef904a536a0a4593b633adc465f15d4ed6
yudahan <103639913+danbi22@users.noreply.github.com>
2023-03-20 10:25:44 +0900

    .

--- Java07_inheritance/src/edu/java/inheritance03/InheritanceMain03.java
+++ Java07_inheritance/src/edu/java/inheritance03/InheritanceMain03.java
@@ -6,6 +6,10 @@ public class InheritanceMain03 {
 		// Car 타입의 객체 생성
 		Car car1 = new Car(100, 30);
 		car1.drive();
+		
+		// HybridCar 타입의 객체 생성
+		HybridCar car2 = new HybridCar(90, 50, 100);
+		car2.drive();
 	}
 
 }
