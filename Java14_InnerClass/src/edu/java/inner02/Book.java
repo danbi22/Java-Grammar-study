7e3750945c4ceeba917913e9810dca5a7ed370ce
yudahan <103639913+danbi22@users.noreply.github.com>
2023-03-31 16:38:39 +0900

    InnerClass

--- Java14_InnerClass/src/edu/java/inner02/Book.java
+++ Java14_InnerClass/src/edu/java/inner02/Book.java
@@ -1,5 +1,23 @@
 package edu.java.inner02;
 
 public class Book {
+	// field
+	private String title; // 책 제목
+	private String author; // 책 저자
+	private String publisher; // 출판사
+		
+	// constructor
+	public Book(String title, String author, String publisher) {
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+	}
 
+	// method
+	@Override
+	public String toString() {
+		return String.format("Book(title=%s, author=%s, publisher=%s)", title, author, publisher);
+	}
+	
+	
 }
