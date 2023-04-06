a75756204e0e1cb2013f40b8644af55fd1091d12
yudahan <103639913+danbi22@users.noreply.github.com>
2023-04-06 20:32:39 +0900

    post

--- Post/src/post02/PostFile.java
+++ Post/src/post02/PostFile.java
@@ -1,11 +1,15 @@
 package post02;
 
 import java.io.File;
+import java.io.IOException;
 
 public class PostFile {
 	// field
 	private static final String FOLDER = "data";
 	private static final String FILE = "data/post.dat";
+	
+	// constructor
+	private PostFile() {}
 	
 	// method
 	public static void makeFolder() {
@@ -24,6 +28,12 @@ public class PostFile {
 		File file = new File(FILE);
 		if (file.exists()) {
 			System.out.println("파일이 이미 있습니다.");
+			return;
+		}
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
