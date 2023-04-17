0917869d3852b5b61631e22220e67778480bdb98
yudahan <103639913+danbi22@users.noreply.github.com>
2023-04-17 09:52:46 +0900

    Update RecorderMain.java
    .

--- Recorder/src/recorderView/RecorderMain.java
+++ Recorder/src/recorderView/RecorderMain.java
@@ -3,27 +3,10 @@ package recorderView;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
-import java.awt.BorderLayout;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-
-public class RecorderMain {
+public class recorderMain {
 
 	private JFrame frame;
-	private JTable table;
-	private JButton btnStatistic;
-	private JScrollPane scrollPane;
-	private JPanel panel_1;
-	private JButton btnCreate;
-	private JPanel panel;
 
 	/**
 	 * Launch the application.
@@ -32,7 +15,7 @@ public class RecorderMain {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					RecorderMain window = new RecorderMain();
+					recorderMain window = new recorderMain();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -44,7 +27,7 @@ public class RecorderMain {
 	/**
 	 * Create the application.
 	 */
-	public RecorderMain() {
+	public recorderMain() {
 		initialize();
 	}
 
@@ -53,48 +36,8 @@ public class RecorderMain {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 662, 484);
+		frame.setBounds(100, 100, 563, 382);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		frame.setTitle("Recorder");
-		
-		panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.NORTH);
-		panel.setLayout(new BorderLayout(0, 0));
-		
-		btnStatistic = new JButton("통계");
-		btnStatistic.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnStatistic.setHorizontalAlignment(SwingConstants.LEFT);
-		panel.add(btnStatistic, BorderLayout.WEST);
-		
-		panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, BorderLayout.SOUTH);
-		
-		btnCreate = new JButton("+");
-		btnCreate.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		panel_1.add(btnCreate);
-		
-		scrollPane = new JScrollPane();
-		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-		
-		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Name", "InitialValue", "GrowthRate", "FinalRate", "Memo"
-			}
-		));
-		scrollPane.setViewportView(table);
 	}
-
-	
 
 }
